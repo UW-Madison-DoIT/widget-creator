8793aba13c44b74e6765d658df44bec62415ac89
Tim Levett <levett@wisc.edu>
2015-07-08 17:30:48 -0500

    MUMMNG-1705 : Update to frame 4.1.0.23

--- my-app-seed-war/src/main/webapp/js/app-config.js
+++ my-app-seed-war/src/main/webapp/js/app-config.js
@@ -1,11 +1,13 @@
 define(['angular'], function(angular) {
 angular.module('app-config', [])
     .constant('SERVICE_LOC', {
-                           'sessionInfo' : 'json/sessionsample.json',
-                           'sidebarInfo' : 'samples/sidebar.json'
-                          })
+       'sessionInfo' : 'json/sessionsample.json',
+       'sidebarInfo' : 'samples/sidebar.json'
+    })
     .constant('NAMES', {
-                           'title' : 'MyUW'
-                        });
+       'title' : 'MyUW',
+       'crest' : 'img/uwcrest_web_sm.png',
+       'crestalt' : 'UW Crest'
+    });
 
 });
