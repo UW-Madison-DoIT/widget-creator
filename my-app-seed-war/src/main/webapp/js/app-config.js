bebccf1081f5d94f2d4d5a92dcfe1aa91e7e5950
Tim Levett <levett@wisc.edu>
2015-09-03 15:39:25 -0500

    actually it is good to have the app-config.js in here

---
+++ my-app-seed-war/src/main/webapp/js/app-config.js
@@ -0,0 +1,40 @@
+define(['angular'], function(angular) {
+
+    var config = angular.module('app-config', []);
+    config
+        .constant('APP_FLAGS', {
+            'features' : false
+        })
+        .constant('SERVICE_LOC', {
+            'sessionInfo' : 'json/sessionsample.json',
+            'sidebarInfo' : 'samples/sidebar.json',
+            'featuresInfo' : 'samples/features.json',
+            'notificationsURL' : 'samples/notifications.json',
+            'kvURL' : null,
+            'groupURL' : null
+        })
+        .constant('NAMES', {
+            'title' : 'MyUW',
+            'ariaLabelTitle' : 'My U W',
+            'crest' : 'img/uwcrest_web_sm.png',
+            'crestalt' : 'UW Crest',
+            'sublogo' : 'seed'
+        })
+        .constant('SEARCH',{
+            'isWeb' : false,
+            'searchURL' : 'https://my.wisc.edu/web/apps/search/'
+        })
+        .constant('NOTIFICATION', {
+            'enabled' : false,
+            'groupFiltering' : false,
+            'notificationFullURL' : 'notifications'
+        })
+        .constant('MISC_URLS',{
+            'feedbackURL' : 'https://my.wisc.edu/portal/p/feedback',
+            'back2ClassicURL' : null,
+            'whatsNewURL' : null
+        });
+
+    return config;
+
+});
