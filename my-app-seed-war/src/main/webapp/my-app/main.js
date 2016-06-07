3bf5157dc7c8aee8ab590588317869ff997b5937
Tim Levett <tim.levett@wisc.edu>
2016-06-07 14:12:17 -0500

    release 2.3.1 and 2.3.0

--- my-app-seed-war/src/main/webapp/my-app/main.js
+++ my-app-seed-war/src/main/webapp/my-app/main.js
@@ -2,9 +2,9 @@ define([
     'angular',
     'require',
     './example/routes', //add all the paths to your routes here
-    'portal/settings/route', // example pulling in portal module routes
+    'portal/settings/routes', // example pulling in portal module routes
     'portal/about/route', // Nice about page for your application
-    'portal/main/routes', 
+    'portal/main/routes',
     'portal',
     'app-config',
     'ngRoute',
@@ -12,7 +12,7 @@ define([
     'ngStorage',
     './example/controllers', //add all your paths to your other js files here
     './example/directives',
-], function(angular, require, exampleRoutes, settingsRoute, aboutRoute, mainRoutes) { //notice each route file is now an object
+], function(angular, require, exampleRoutes, settingsRoutes, aboutRoute, mainRoutes) { //notice each route file is now an object
 
     var app = angular.module('my-app', [
         'app-config',
@@ -30,7 +30,8 @@ define([
         $routeProvider.
             when('/default', exampleRoutes.default). //use your route object to get the templateurl
             when('/view2', exampleRoutes.second).
-            when('/settings', settingsRoute).
+            when('/settings', settingsRoutes.betaSettings).
+            when('/user-settings', settingsRoutes.userSettings).
             when('/about', aboutRoute).
             when('/access-denied', mainRoutes.accessDenied).
             otherwise({ redirectTo : '/default'});
