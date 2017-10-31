969b0e7b47aea17be1619d151444b3804e206f46
Christian Murphy <cmurphy@unicon.net>
2017-10-30 20:36:31 -0700

    chore(package): update packages

--- src/main/webapp/karma.conf.js
+++ src/main/webapp/karma.conf.js
@@ -1,58 +1,58 @@
 /* eslint-env node */
+
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function(config) {
-    config.set({
+  config.set({
+    basePath: './',
 
-        basePath: './',
+    files: [
+      // main.js will handle finding and loading the tests,
+      // by way of RequireJS.
+      'test/main.js',
+      '../../node_modules/karma-read-json/karma-read-json.js',
+      // all other files need to be listed in order to be hosted and
+      // available, but excluded so that they are not run automatically.
+      {pattern: './**', included: false},
+    ],
 
-        files: [
-            // main.js will handle finding and loading the tests,
-            // by way of RequireJS.
-            'test/main.js',
-            '../../node_modules/karma-read-json/karma-read-json.js',
-            // all other files need to be listed in order to be hosted and
-            // available, but excluded so that they are not run automatically.
-            {pattern: './**', included: false},
-        ],
+    preprocessors: {
+      'portal/**/*.js': 'coverage',
+      'my-app/**/*.js': 'coverage',
+    },
 
-        preprocessors: {
-          'portal/**/*.js': 'coverage',
-          'my-app/**/*.js': 'coverage',
-        },
+    autoWatch: true,
 
-        autoWatch: true,
+    frameworks: ['jasmine', 'requirejs'],
 
-        frameworks: ['jasmine', 'requirejs'],
+    browsers: ['ChromeHeadless'], // or 'Chrome'
 
-        browsers: ['PhantomJS'], // or 'Chrome'
+    plugins: [
+      'karma-htmlfile-reporter',
+      'karma-chrome-launcher',
+      'karma-jasmine',
+      'karma-requirejs',
+      'karma-coverage',
+      'karma-coveralls',
+    ],
 
-        plugins: [
-            'karma-htmlfile-reporter',
-            'karma-chrome-launcher',
-            'karma-phantomjs-launcher',
-            'karma-jasmine',
-            'karma-requirejs',
-            'karma-coverage',
-            'karma-coveralls',
-        ],
+    reporters: ['dots', 'html', 'coverage', 'coveralls'],
 
-        reporters: ['dots', 'html', 'coverage', 'coveralls'],
+    htmlReporter: {
+      outputFile: 'test_out/units.html',
+    },
 
-        htmlReporter: {
-          outputFile: 'test_out/units.html',
-        },
+    junitReporter: {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit',
+    },
 
-        junitReporter: {
-            outputFile: 'test_out/unit.xml',
-            suite: 'unit',
-        },
+    coverageReporter: {
+      // lcov or lcovonly are required for generating lcov.info files
+      type: 'lcov',
+      dir: 'coverage/',
+    },
 
-        coverageReporter: {
-          // lcov or lcovonly are required for generating lcov.info files
-          type: 'lcov',
-          dir: 'coverage/',
-        },
-
-        colors: true,
-
-    });
+    colors: true,
+  });
 };
